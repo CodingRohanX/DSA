604061fe694d0e847e171d065b43bd82a64018d5
Rohan Ravi <157512619+CodingRohanX@users.noreply.github.com>
2025-05-17 20:05:15 +0530

    Time: 151 ms (88.42%) | Memory: 132.6 MB (9.72%) - LeetSync

--- 204-count-primes/count-primes.cpp
+++ 204-count-primes/count-primes.cpp
@@ -5,13 +5,15 @@ public:
         std::vector<int> v(n, 1);
         if (n <= 2) return 0;
         v[0] = 0, v[1] = 0;
-        for(int j=2;j<n;j++){
-            for(int i=2;j*i<n;i++){
-                v[j*i]=0;
+        for (int j = 2; j * j < n; j++) { 
+            if (v[j]) { 
+                for (int i = j * j; i < n; i += j) {
+                    v[i] = 0;
+                }
             }
         }
-        for(auto i: v){
-            if(i==1) c++;
+        for (int i : v) {
+            if (i == 1) c++;
         }
         return c;
     }
