79205674e838d71c71cd9e4850c920ca99f9cbf2
Rohan Ravi <157512619+CodingRohanX@users.noreply.github.com>
2025-08-05 20:34:36 +0530

    Time: 32 ms (81.47%) | Memory: 79.1 MB (8.01%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.cpp
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -3,7 +3,6 @@ public:
     int totalFruit(vector<int>& f) {
         map<int,int> m;
         int mx=1,p1=0,p2=0;
-        //m[f[0]]++;
         while(p2<f.size()){
             m[f[p2]]++;
             if(m.size()>2){
