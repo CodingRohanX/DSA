f3e769ebd2bebf2694ca7533388a6e36e3909818
Rohan Ravi <157512619+CodingRohanX@users.noreply.github.com>
2025-08-16 10:32:08 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (41.82%) - LeetSync

---
+++ 1448-maximum-69-number/maximum-69-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maximum69Number (int n) {
+        string s=to_string(n);
+        for(auto &i : s){
+            if(i=='6'){
+                i='9';
+                break;
+            }
+        }
+        return stoi(s);
+    }
+};
