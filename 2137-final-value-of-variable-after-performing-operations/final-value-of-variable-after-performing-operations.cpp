a6d81491e56b66694534cf70cd053ae19d795f4c
Rohan Ravi <157512619+CodingRohanX@users.noreply.github.com>
2025-10-20 17:51:14 +0530

    Time: 2 ms (11.87%) | Memory: 17.6 MB (42.92%) - LeetSync

---
+++ 2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& o) {
+        int a=0;
+        for(auto i: o){
+            for(auto f: i){
+                if(f=='+'){
+                    a++;
+                    break;
+                }
+                else if(f=='-'){
+                    a--;
+                    break;
+                }
+            }
+        }
+        return a;
+    }
+};
