83d4a7936cb15d1787c455241950a99dbb242fba
Rohan Ravi <157512619+CodingRohanX@users.noreply.github.com>
2026-01-11 05:23:33 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (14.35%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -10,10 +10,28 @@ public:
         return dp[i][j];
     }
 
+    int solvetabu(int m, int n, vector<vector<int>> &dp,vector<vector<int>> &g){
+        dp[0][0]=g[0][0];
+        int l=0,u=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(!(i==0 && j==0)){
+                    l=INT_MAX;u=INT_MAX;
+                    if(j!=0) l=dp[i][j-1];
+                    if (i!=0) u=dp[i-1][j];
+                    dp[i][j]=min(l,u);
+                    dp[i][j]+=g[i][j];
+                }
+            }
+        }	
+        return dp[m-1][n-1];
+    }
+
     int minPathSum(vector<vector<int>>& grid) {
         int m=grid.size();
         int n=grid[0].size();
         vector<vector<int>> dp(m, vector<int>(n, -1));
-        return solve(m,n,0,0,dp,grid);
+        //return solve(m,n,0,0,dp,grid);
+        return solvetabu(m,n,dp,grid);
     }
 };
