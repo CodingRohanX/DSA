c2a468e2ae81f5cdd199d753a5b3ea8d588c5848
Rohan Ravi <157512619+CodingRohanX@users.noreply.github.com>
2026-09-21 14:36:35 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (29.90%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int solve(int m, int n,int i,int j, vector<vector<int>> &dp){
+    int solve(int m, int n, int i, int j, vector<vector<int>> &dp){
         if(i==m) return 0;
         if(j==n) return 0;
         if(i==m-1 && j==n-1) return 1;
@@ -9,25 +9,9 @@ public:
         return dp[i][j];
     }
 
-    int solvetabu(int m, int n, vector<vector<int>> &dp){
-        dp[0][0]=1;
-        int l=0,u=0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(!(i==0 && j==0)){
-                    l=0;u=0;
-                    if(j!=0) l=dp[i][j-1];
-                    if (i!=0) u=dp[i-1][j];
-                    dp[i][j]=l+u;
-                }
-            }
-        }	
-        return dp[m-1][n-1];
-    }
-
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-	    //return solve(m,n,0,0,dp);
-        return solvetabu(m,n,dp);
+        vector<vector<int>> dp(m, vector<int>(n,-1));
+        int ans = solve(m,n,0,0,dp);
+        return ans;
     }
 };
